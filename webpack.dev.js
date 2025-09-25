56f989497b6808bd49979624b4394dd7cefacc49
HagaiLevHacohen <levhacohen@mail.tau.ac.il>
2025-09-25 16:55:09 +0300

    improved version with webpack-merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,42 +1,15 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+module.exports = merge(common, {
+  mode: 'development',
   devtool: "eval-source-map",
   devServer: {
+    static: {
+    directory: path.resolve(__dirname, 'dist'),
+    },
     watchFiles: ["./src/template.html"],
+    open: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-};
+});
